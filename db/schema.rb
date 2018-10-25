0ac04b2b1562e77f3085b626b89fd4154246a9f5
Voxoff <guybennettjones@hotmail.co.uk>
2018-10-25 11:52:48 +0100

    some serious frontend bish bash bosch, banners with pngs, product and education pages

--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2018_10_15_104751) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "active"
+    t.boolean "active", default: true
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,11 @@ Product.destroy_all
 User.destroy_all
 puts 'Creating products...'
 
-p = Product.create!(price: 1, name: 'Lemon', remote_photo_url: 'https://pbs.twimg.com/media/B_AUcKeU4AE6ZcG.jpg:large')
-Product.create!(price: 3, name: 'Orange', remote_photo_url: 'https://cdn-ak.f.st-hatena.com/images/fotolife/s/suzumidokoro/20160413/20160413220730.jpg')
-Product.create!(price: 2, name: 'Chocolate', remote_photo_url: 'https://cdn-ak.f.st-hatena.com/images/fotolife/s/suzumidokoro/20160413/20160413220730.jpg')
-Product.create!(price: 1, name: 'Pure', remote_photo_url: 'https://pbs.twimg.com/media/B_AUcKeU4AE6ZcG.jpg:large')
-Product.create!(price: 3, name: 'Mint', remote_photo_url: 'https://cdn-ak.f.st-hatena.com/images/fotolife/s/suzumidokoro/20160413/20160413220730.jpg')
+p = Product.create!(price: 1, name: 'Lemon', remote_photo_url: 'https://floydsofleadville.com/wp-content/uploads/2018/02/FOL_CBD_Softgels_FullSpectrum_50mg_Front_1200-compressor.jpg')
+Product.create!(price: 3, name: 'Orange', remote_photo_url: 'https://floydsofleadville.com/wp-content/uploads/2018/02/FOL_CBD_Softgels_FullSpectrum_50mg_Front_1200-compressor.jpg')
+Product.create!(price: 2, name: 'Chocolate', remote_photo_url: 'https://floydsofleadville.com/wp-content/uploads/2018/02/FOL_CBD_Softgels_FullSpectrum_50mg_Front_1200-compressor.jpg')
+Product.create!(price: 1, name: 'Pure', remote_photo_url: 'https://floydsofleadville.com/wp-content/uploads/2018/02/FOL_CBD_Softgels_FullSpectrum_50mg_Front_1200-compressor.jpg')
+Product.create!(price: 3, name: 'Mint', remote_photo_url: 'https://floydsofleadville.com/wp-content/uploads/2018/02/FOL_CBD_Softgels_FullSpectrum_50mg_Front_1200-compressor.jpg')
 
 
 user = User.create!(email: "admin@admin.com", password: "123123")
