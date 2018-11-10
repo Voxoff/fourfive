5b3d20e047f902a7d63bc0a3e828c6bb05da48fd
Voxoff <guybennettjones@hotmail.co.uk>
2018-11-10 22:49:30 +0000

    pages urls

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
-  get 'pages/about'
-  get 'pages/education'
-  get 'pages/privacy_policy'
+  get 'about', to: 'pages#about'
+  get 'education', to: 'pages#education'
+  get 'privacy_policy', to: 'pages#privacy_policy'
   resources :products, only: [:show, :index]
   resources :cart_items, only: [:show, :create, :index] do
   end
   resources :cart, only: [:show] do
     resources :payments, only: [:new, :create]
   end
-  resources :reviews
+  resources :reviews, only: :index
 end
