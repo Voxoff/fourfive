2d29e03101687181036d703950ac2689b249c8d5
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2018-11-19 16:03:37 +0000

    ecommerce  to flow in admin

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -3,7 +3,7 @@ def authenticate_admin!
 end
 
 ActiveAdmin.setup do |config|
-  config.site_title = "Ecommerce Template"
+  config.site_title = "FlowCBD"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
