b67f058b5a510daa39e4b4cba5ec0eec25bf4919
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2018-12-02 22:09:15 +0000

    updated education page and style of accordion. updated controller to be shorter for cartitmes

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,22 +1,17 @@
 class CartItemsController < ApplicationController
   def create
     @cart = Cart.find(cart_params[:cart_id])
-    @cart_item =  @cart.cart_items.find {|i| i.product.id == cart_params[:product_id].to_i}
+    @cart_item = @cart.cart_items.find {|i| i.product.id == cart_params[:product_id].to_i}
     if @cart_item
-      new_quantity = @cart_item.quantity + params[:quantity].to_i
-      @cart_item.quantity = new_quantity
+      @cart_item.quantity = @cart_item.quantity + params[:quantity].to_i
     else
       @cart_item = CartItem.new(cart_params)
-      @cart_item.product = Product.find(cart_params[:product_id])
-      @cart_item.quantity = cart_params[:quantity]
-      # @cart_item.strength = Strength.where(strength: cart_items_params[:strength]).first
     end
-      if @cart_item.save
-        flash[:notice] = "That's been added to your cart!"
-      else
-        flash[:notice] = "There was an error. Sorry!"
-      end
-
+    if @cart_item.save
+      flash[:notice] = "That's been added to your cart!"
+    else
+      flash[:notice] = "There was an error. Sorry!"
+    end
     redirect_back(fallback_location: cart_path(@cart.id))
   end
 
