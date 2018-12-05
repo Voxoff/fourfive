db95689d542af691a05e92755cf12b1e85de71bc
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2018-12-05 16:22:34 +0000

    success page to follow transaction. Yay. WOooo

--- README.md
+++ README.md
@@ -16,6 +16,12 @@ what happens on checkout
 admin side after checkout
 mobile responsiveness
 
+
 <!-- carts guest logging -->
 
 the cartoon out story needs to be put in cludinary
+
+so everything is good when it goes to zion. Things that need to change are the cart and its status.
+All old carts can be old orders?
+But I need to check status after zion has done its thing(home_page or redirect page) and then change the
+cart status and ??
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,31 +3,31 @@ class PaymentsController < ApplicationController
   before_action :get_cart
 
   def checkout
-
     @amount = @cart.amount
     @cart_items = @cart.cart_items
     user = current_or_guest_user
+    coupon = checkout_params[:coupon]
     if @cart_items.empty?
       flash[:notice] = "You need to put items in your cart in order to buy them!"
-      redirect_to root_path
+      return redirect_to root_path
     elsif user.cart != @cart
       flash[:notice] = "Error!"
-      redirect_to root_path
+      return redirect_to root_path
     end
-    @address = Address.new(address_params)
-    @address.user = guest_user
-    flash[:notice] = "Address was invalid" if !@address.save
-    if user.guest && checkout_params[:email]
-      guest_user.email = checkout_params[:email]
-    end
-    coupon = checkout_params[:coupon]
-    if coupon.present? && !coupons.include?(coupon)
-      flash[:notice] = "That coupon code did not work"
-      redirect_to new_cart_payment_path
-    else
+    if coupon.present? && coupons.include?(coupon)
       flash[:notice] = "Coupon successfully applied."
       @amount = @amount.to_f * 0.9
+    elsif coupon.present? && !coupons.include?(coupon)
+      flash[:notice] = "That coupon code did not work"
+      return redirect_to new_cart_payment_path
     end
+    @address = Address.new(address_params)
+    @address.user = user #check if the user already has an identical address with validations
+    flash[:notice] = "Address was invalid" unless @address.save
+    if user.guest? && checkout_params[:email]
+      guest_user.email = checkout_params[:email]
+    end
+    # Now we need to store the cart, change it to false and create a new cart
     @checkoutId = zion
   end
 
@@ -35,9 +35,36 @@ class PaymentsController < ApplicationController
     @cart_items = @cart.cart_items
     if @cart_items.empty?
       flash[:notice] = "You need to put items in your cart in order to buy them!"
-      redirect_to root_path
+      return redirect_to root_path
     end
   end
+
+  def success
+    #params => id=0033E3B0DA7F4867166FCB43FBB0FF0F.uat01-vm-tx04&
+              #resourcePath=%2Fv1%2Fcheckouts%2F0033E3B0DA7F4867166FCB43FBB0FF0F.uat01-vm-tx04%2Fpayment
+    require 'net/https'
+    require 'uri'
+    require 'json'
+  	path = ("?authentication.userId=8a8294174b7ecb28014b9699220015cc" +
+		"&authentication.password=sy6KJsT8" +
+		"&authentication.entityId=8a8294174b7ecb28014b9699220015ca")
+	  uri = URI.parse('https://test.oppwa.com/v1/checkouts/{id}/payment' + path)
+	  http = Net::HTTP.new(uri.host, uri.port)
+	  http.use_ssl = true
+	  res = http.get(uri.request_uri)
+    return JSON.parse(res.body)
+    #     {
+    #   "result":{
+    #     "code":"200.300.404",
+    #     "description":"invalid or missing parameter - (opp) No payment session found for the requested id - are you mixing test/live servers or have you paid more than 30min ago?"
+    #   },
+    #   "buildNumber":"7469d0e5bd2dccca50bbd107625279e76a2c9ff3@2018-12-04 10:59:49 +0000",
+    #   "timestamp":"2018-12-05 16:13:13+0000",
+    #   "ndc":"8a8294174b7ecb28014b9699220015ca_844fa63b992e44a393277185e4aa7b2b"
+    # }
+  end
+
+
 
   private
 
@@ -51,6 +78,10 @@ class PaymentsController < ApplicationController
 
   def coupons
     ["DOMDAY", "COUPON"]
+  end
+
+  def success_params
+    params.permit(:id, :resourcePath)
   end
 
   def zion
