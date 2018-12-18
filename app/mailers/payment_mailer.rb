7ac460da9277e19ee37ffa444258e20afb013a6f
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2018-12-18 13:06:48 +0000

    terms and conditions

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -4,4 +4,12 @@ class PaymentMailer < ApplicationMailer
     @user = user
     mail(to: @user.email, subject: "Receipt") if @user.email
   end
+
+  private
+
+  def add_pdf
+    #https://stackoverflow.com/questions/21551435/rails-4-add-mail-attachment-as-remote-pdf-file
+    file = generate
+    attachments["receipt.pdf"] = file.read
+  end
 end
