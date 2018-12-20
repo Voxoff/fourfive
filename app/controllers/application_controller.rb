96a776c909bc0b1b4de633c620f8b434a3682b75
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2018-12-20 18:07:02 +0000

    mobile images

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,6 +36,22 @@ class ApplicationController < ActionController::Base
     guest_user if with_retry
   end
 
+  before_action :prepare_for_mobile
+
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+  helper_method :mobile_device?
+
+  def prepare_for_mobile
+    session[:mobile_param] = params[:mobile] if params[:mobile]
+    request.format = :mobile if mobile_device?
+  end
+
   private
 
   # def skip_pundit?
