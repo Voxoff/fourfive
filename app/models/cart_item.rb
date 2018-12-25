0f85ea90aa7d63128fd765f3100e7dffffd04ede
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2018-12-25 22:58:05 +0000

    temp fix for quantity of 0 cartitems

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,9 +1,16 @@
 class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
-  validates :quantity, presence: true
+  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
   # belongs_to :strength
+
+  after_save :check_quantity
+
   def line_cost
     quantity * product.price
+  end
+
+  def check_quantity
+    destroy if quantity.zero?
   end
 end
