be512c9308dde09b93e89288b6950033e63d31ac
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2019-01-05 15:25:29 +0000

    delete old users updated

--- app/jobs/delete_old_users_job.rb
+++ app/jobs/delete_old_users_job.rb
@@ -5,7 +5,8 @@ class DeleteOldUsersJob < ApplicationJob
     # get all users that do not have a cart and have not been updated for a week
     users = User.left_outer_joins(:carts)
                 .where(carts: { user_id: nil })
-                .where("users.updated_at <= ?", Time.now - 7.days)
-    destroy ? users.destroy_all : users
+                .where("users.updated_at <= ?", Time.now - 6.days).pluck(:id)
+    destroy ? User.delete(users) : users.count
+    # beware NO CALLBACK
   end
 end
