6547f9e860d09721edfe462cbb2912d8e48be54f
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2019-01-08 16:29:18 +0000

    heroku production

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -2,12 +2,11 @@ class PaymentMailer < ApplicationMailer
 
   def success(email, email_hash)
     add_pdf(email_hash)
-    # if Rails.env.development?
+    if Rails.env.development?
       mail(to: "guy@fourfivecbd.co.uk", subject: "Receipt") if email
-    # else
-      # mail(to: email, subject: "Receipt") if email
-    # end
-
+    else
+      mail(to: email, subject: "Receipt") if email
+    end
   end
 
   def order(pdf)
@@ -22,11 +21,6 @@ class PaymentMailer < ApplicationMailer
   private
 
   def add_pdf(email_hash)
-    # t = Tempfile.create do |f|
-    #   pdf.render_file f
-    #   f.flush
-    #   File.read(f)
-    # end
     attachments["receipt.pdf"] = InvoicePdf.new(email_hash).render
   end
 end
