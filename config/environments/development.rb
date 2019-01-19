bafb1d02447c2d4c5f73f5c55b3d174b7aec1a62
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2019-01-19 20:33:11 +0000

    why not use p tags?

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,13 +61,13 @@ Rails.application.configure do
   config.active_record.migration_error = :page_load
 
   # Highlight code that triggered database queries in logs.
-  config.active_record.verbose_query_logs = false
+  config.active_record.verbose_query_logs = true
   config.log_level = :debug
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = false
+  config.assets.debug = true
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
