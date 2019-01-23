ae3b32c90c90a323e8ea86ebcf11e2f90513d772
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2019-01-23 20:57:31 +0000

    ordered products

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,5 +1,4 @@
 ActiveAdmin.register_page "Dashboard" do
-  # includes :address
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
@@ -11,14 +10,14 @@ ActiveAdmin.register_page "Dashboard" do
           table do
             thead do
               tr do
-                 Product.all.map(&:specific_name).each &method(:th)
+                Product.order(:id).map(&:specific_name).each &method(:th)
               end
             end
             tbody do
               tr do
                 (1..Product.count).to_a.each do |id|
                   td do
-                    cart_item.joins(:product).merge(Product.where(id: id)).count    
+                    cart_item.joins(:product).merge(Product.where(id: id)).count
                   end
                 end
               end
@@ -30,7 +29,6 @@ ActiveAdmin.register_page "Dashboard" do
       column do
         panel "Last 10 Orders" do
           table_for Cart.includes(:address, cart_items: :product).orders.last(10).reverse do
-            # column("Customer") { |cart| link_to(cart.user.email, admin_user_path(cart.user)) }
             column("Customer name") { |cart| link_to(cart.address&.full_name, admin_cart_path(cart)) }
             column("Total") { |cart| number_to_currency(cart.amount, unit: "£") }
             column("Created at") {|c| c.checked_out_at ? c.checked_out_at.strftime("%A, %b %d %H:%M") : c.updated_at.strftime("%A, %b %d %H:%M") }
