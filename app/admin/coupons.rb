1971f925c00b6d70639b68a3c1ec27cf7f26d097
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2019-01-25 19:37:26 +0000

    got rid of confusing carts part on coupons

--- app/admin/coupons.rb
+++ app/admin/coupons.rb
@@ -5,18 +5,18 @@ ActiveAdmin.register Coupon do
     selectable_column
     id_column
     column :code
-    column "No. of orders" do |coupon|
-      coupon.carts.orders.count
-    end
     column "Discount" do |coupon|
       coupon.discount.to_s + "%"
+    end
+    column "No. of orders" do |coupon|
+      coupon.carts.orders.count
     end
     column "Money spent on coupons" do |c|
       (c.carts.orders.map(&:amount).reduce(:+) * c.percent) unless c.carts.orders.empty?
     end
-    column "No. of carts" do |coupon|
-      coupon.carts.count
-    end
+    # column "No. of carts (not all checked out)" do |coupon|
+    #   coupon.carts.count
+    # end
   end
 
   form do |f|
