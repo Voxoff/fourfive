2e21a158dae00963411f05a3fa9f4a99ca52ec9f
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2019-01-26 13:23:34 +0000

    active admin callback

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,9 +1,10 @@
 class Coupon < ApplicationRecord
   has_many :carts
-  validates :code, presence: true
+  validates :code, presence: true, uniqueness: true
   validates :discount, presence: true, numericality: { only_integer: true }
 
-  before_validation {self.code = self.code.upcase! }
+  include ActiveAdmin::Callbacks
+  before_validation { self.code = self.code.upcase }
 
   def percent
     discount.to_f / 100
