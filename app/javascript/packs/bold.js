4abe6d805f34cf4fb4f59fd07b30cebe0d80f615
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2019-01-28 20:41:18 +0000

    so the javascript were for reviews which have gone for now

--- app/javascript/packs/bold.js
+++ app/javascript/packs/bold.js
@@ -1,12 +1,9 @@
-console.log(tag)
-console.log("tag")
-
 function boldString(str, find){
-      var re = new RegExp(find, 'g');
-      return str.replace(re, '<b>'+find+'</b>');
-  }
-  let strings = document.querySelectorAll(".text")
-  strings.forEach((i) => {
-    res = boldString(i.innerHTML, "fourfive")
-    i.innerHTML = res
-  })
+  var re = new RegExp(find, 'g');
+  return str.replace(re, '<b>'+find+'</b>');
+}
+let strings = document.querySelectorAll(".text")
+strings.forEach((i) => {
+  res = boldString(i.innerHTML, "fourfive")
+  i.innerHTML = res
+})
