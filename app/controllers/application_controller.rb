774a402685fa9b438a6092febb13b5dcae34f8d2
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2019-01-31 14:32:04 +0000

    don't want none of those pesky errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -50,7 +50,7 @@ class ApplicationController < ActionController::Base
   end
 
   def create_guest_user
-    u = User.new(guest: true, email: "guest_#{Time.now.to_i}#{rand(100)}@example.com")
+    u = User.new(guest: true, email: "guest_#{Time.now.to_i}#{rand(1000)}@example.com")
     u.save!(validate: false)
     session[:guest_user_id] = u.id
     u
