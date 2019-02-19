496291569b7b6ac635c58aaac0228782b88aa221
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2019-02-19 19:36:22 +0000

    update chart to ignore pre jan 1 orders for better calibration

--- app/admin/order_chart.rb
+++ app/admin/order_chart.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register_page "Order Chart" do
   content do
-    @metric = Cart.orders.group_by_day(:checked_out_at).count
+    @metric = Cart.orders.where("created_at >= ?", Date.parse("3 January 2019")).group_by_day(:checked_out_at).count
     render partial: 'charts/orders', locals: { metric: @metric }
   end
 end
