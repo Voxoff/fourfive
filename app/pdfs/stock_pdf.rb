276353de29ee750f3a83aae303e1e2a4f3b48d75
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2019-02-22 22:29:24 +0000

    invoice

--- app/pdfs/stock_pdf.rb
+++ app/pdfs/stock_pdf.rb
@@ -5,19 +5,19 @@ class StockPdf < Prawn::Document
                         .merge(Cart.orders.month_of("January"))
     @products = Product.order(:id)
     @oil = [@products.select(&:oil?).map(&:oil_name).unshift("Product")]
-    @capsules_and_balm = [@products.reject(&:oil?).map(&:specific_name).unshift("Product")]
+    @capsule_and_balm = [@products.reject(&:oil?).map(&:specific_name).unshift("Product")]
     @hash = cart_item.group(:product_id).count
     @oil_count = product_count("oil")
-    @capsules_and_balm_count = product_count("capsules")
+    @capsule_and_balm_count = product_count("capsule_and_balm")
     @oil_revenue = product_revenue("oil")
-    @capsules_and_balm_revenue = product_revenue("capsules")
-    @oil_total = product_total(@oil_revenue)
-    @capsules_total = product_total(@capsules_and_balm_revenue)
+    @capsule_and_balm_revenue = product_revenue("capsule_and_balm")
+    @oil_total = [product_total(@oil_revenue)]
+    @capsule_and_balm_total = [product_total(@capsule_and_balm_revenue)]
     create
   end
 
   def product_total(var)
-    var.first.drop(1).map{|i| i.tr("£", "").to_f}.reduce(:+).to_i
+    ["Total", "£" + var.first.drop(1).map{ |i| i.tr("£", "").to_f}.reduce(:+).to_i.to_s ]
   end
 
   def oil?(str)
@@ -30,45 +30,58 @@ class StockPdf < Prawn::Document
 
   def product_count(str)
     id_range = id_range(str)
-    [id_range.to_a.map { |id| @hash[id] || "0" }.unshift("Quantity")]
+    count = id_range.to_a.map { |id| @hash[id] || "0" }
+    [count.unshift("Quantity")]
   end
 
   def product_revenue(str)
     id_range = id_range(str)
-    [id_range.to_a.map do |id|
+    revenue = id_range.to_a.map do |id|
       @hash[id] ? "£#{@hash[id] * @products.find([id]).first.price}" : "£0"
-    end.unshift("Price")]
+    end
+    [revenue.unshift("Revenue")]
   end
 
   def tables(str)
     table([["#{str}s"].map{|i| i.capitalize.tr("_", " ")}], position: :left) do
-      style(row(0..-1).columns(0..-1), padding: [10,10,10,10], width: 160, borders: [], background_color: 'e9e9e9', border_color: 'dddddd', align: :center)
+      style(row(0..-1).columns(0..-1), padding: [10,10,3,10], width: 160, borders: [], background_color: 'e9e9e9', border_color: 'dddddd', align: :center)
     end
 
-    table(self.instance_variable_get(:"@#{string}"), width: bounds.width) do
-      style(row(0..-1).columns(0), padding: [5,5,5, 5], align: :left)
+    table(self.instance_variable_get(:"@#{str}"), width: bounds.width) do
+      style(row(0..-1).columns(0), padding: [5,5,5, 5], align: :left, borders: [], background_color: 'e9e9e9', border_color: 'dddddd')
       style(row(0..-1).columns(1..-1), borders: [], padding: [5,5,5,5], background_color: 'e9e9e9', border_color: 'dddddd', align: :center)
     end
 
-    table(self.instance_variable_get(:"@#{string}_count"), width: bounds.width) do
-      style(row(0..-1).columns(0), align: :left, padding: [5,5,5,0])
-      style(row(0..-1).columns(1..-1),padding: [5,20,5, 20])
+    table(self.instance_variable_get(:"@#{str}_count"), width: bounds.width) do
+      style(row(0..-1).columns(0), align: :left, padding: [5,0,5,5], borders: [])
+      if str == "oil"
+        style(row(0..-1).columns(1), align: :center, padding: [5,20,5, 0], borders: [])
+        style(row(0..-1).columns(2..-1), align: :center, padding: [5,20,5, 20], borders: [])
+      else
+        style(row(0..-1).columns(1..-1), align: :center, padding: [5,20,5, 20], borders: [])
+      end
     end
 
-     table(self.instance_variable_get(:"@#{string}_revenue"), width: bounds.width) do
-      style(row(0..-1).columns(0), align: :left, padding: [5,5,5,5])
-      style(row(0..-1).columns(1..-1), padding: [5,5,5,5])
+     table(self.instance_variable_get(:"@#{str}_revenue"), width: bounds.width) do
+      style(row(0..-1).columns(0), align: :left, padding: [5,5,5,5], borders: [])
+      if str == "oil"
+        style(row(0..-1).columns(1), align: :left,padding: [5,0,5,10], borders: [:top], border_width: 1, border_color: 'e9e9e9')
+        style(row(0..-1).columns(2..-1), align: :center,padding: [5,5,5,5], borders: [:top], border_width: 1, border_color: 'e9e9e9')
+      else
+        style(row(0..-1).columns(1..-1), align: :center,padding: [5,5,5,5], borders: [:top], border_width: 1, border_color: 'e9e9e9')
+      end
     end
 
-    table(self.instance_variable_get(:"@#{string}_total"), width: bounds.width) do
-      style(row)
+    table(self.instance_variable_get(:"@#{str}_total"), width: bounds.width) do
+      style(row(0..1).columns(0), align: :left, borders: [:top], border_width: 1, border_color: 'e9e9e9', padding: [5, 5, 5, 5])
+      style(row(0..1).columns(1), align: :right, borders: [:top], border_width: 1, border_color: 'e9e9e9', padding: [5, str == "oil" ? 20 : 50, 5, 0])
     end
   end
 
   def create
     initial_y = cursor
     initialmove_y = 5
-    address_x = 35
+    address_x = 15
     invoice_header_x = 325
     lineheight_y = 12
     font_size = 10
@@ -104,11 +117,11 @@ class StockPdf < Prawn::Document
 
     move_down 40
 
-    tables("capsules_and_balm")
+    tables("capsule_and_balm")
     # table([["Capsules & Balms"]]) do
     #   style(row(0..-1).columns(0..-1), :borders => [], :background_color => 'e9e9e9', :border_color => 'dddddd')
     # end
-    # table(@capsules_and_balm, :width => bounds.width) do
+    # table(@capsule_and_balm, :width => bounds.width) do
     #   # style(row(1..-1).columns(0..-1), :padding => [4, 5, 4, 5], :borders => [:bottom], :border_color => 'dddddd')
     #   # style(row(0), :background_color => 'e9e9e9', :border_color => 'dddddd', :font_style => :bold)
     #   # style(row(0).columns(0..-1), :borders => [:top, :bottom])
