1664ea7dfa7e264131bb93fc79548c539ce1416e
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2019-02-22 13:42:55 +0000

    improved homepage sql performance

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,13 @@
 class PagesController < ApplicationController
   include CartControllable
   before_action :find_cart
+
   def home
     @disable_stripe = @disable_top_margin = @alert_no_margin = true
     @user = current_or_guest_user
-    @product_groups = ProductGroup.all.sort
+    @product_groups = ProductGroup.includes(:products).order(:id)
     @cart_item = CartItem.new
-    @reviews = Review.all.take(3)
+    # @reviews = Review.all.take(3)
   end
 
   def about
