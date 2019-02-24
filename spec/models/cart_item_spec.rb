d32302d762947e0c8f33a0d436b630d2a7618a40
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2019-02-24 22:41:13 +0000

    more and more tests

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe CartItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  cart_item = FactoryBot.create(:cart_item)
+  single_capsules_cart_item = FactoryBot.create(:single_capsules_cart_item)
+  context "general" do
+    describe "#line_cost" do
+      it "calculates cost" do
+        expect(cart_item.line_cost).to eq Money.new(29990)
+        expect(single_capsules_cart_item.line_cost).to eq Money.new(2999)
+      end
+    end
+
+
+    describe "#line_cost" do
+      it "calculates cost" do
+        expect(cart_item.line_cost).to eq Money.new(29990)
+      end
+    end
+
+    describe "#update_or_destroy" do
+      it "destroys self if new quantity is zero or lower" do
+        # best i can do for now
+        expect { [cart_item.update_or_destroy(-10), cart_item.reload] }.to raise_error ActiveRecord::RecordNotFound
+      end
+
+      it "updates self if quantity is > 0" do
+        # best i can do for now
+        expect(cart_item.update_or_destroy(10)).to eq true
+      end
+    end
+  end
 end
