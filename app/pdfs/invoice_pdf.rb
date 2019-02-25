d9da6f2b8f93c234d7dde3c1d7fef719fb70cea1
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2019-02-25 15:08:58 +0000

    refactored invoice thru monkeypatch

--- app/pdfs/invoice_pdf.rb
+++ app/pdfs/invoice_pdf.rb
@@ -1,3 +1,4 @@
+require_relative './document.rb'
 class InvoicePdf < Prawn::Document
   def initialize(attributes)
     super(optimize_objects: true, compress: true)
@@ -16,8 +17,8 @@ class InvoicePdf < Prawn::Document
       product = item.product
       @invoice_services_data << [product.readable_name,
                                  product.specific_name,
-                                 product.price,
-                                 item.quantity,
+                                 product.price.to_s,
+                                 item.quantity.to_s,
                                  item.line_cost].map(&:to_s)
     end
 
@@ -43,9 +44,9 @@ class InvoicePdf < Prawn::Document
     # logopath = 'logo.png'
     initial_y = cursor
     initialmove_y = 5
-    address_x = 35
+    @address_x = 35
     invoice_header_x = 325
-    lineheight_y = 12
+    @lineheight_y = 12
     font_size = 10
 
     move_down initialmove_y
@@ -54,22 +55,13 @@ class InvoicePdf < Prawn::Document
     # font "Helvetica"
     font_size font_size
 
-    #start with EON Media Group
-    text_box "fourfive,", :at => [address_x,  cursor]
-    move_down lineheight_y
-    text_box "Flow Nutrition Ltd,", :at => [address_x,  cursor]
-    move_down lineheight_y
-    text_box "2 Victoria Square,", :at => [address_x,  cursor]
-    move_down lineheight_y
-    text_box "Victoria Street,", :at => [address_x,  cursor]
-    move_down lineheight_y
-    text_box "St. Albans, UK, AL1 3TF", :at => [address_x,  cursor]
-    move_down lineheight_y
+    many_box(["fourfive", "Flow Nutrition Ltd,", "2 Victoria Square,", "Victoria Street,", "St. Albans, UK, AL1 3TF"])
+
 
     last_measured_y = cursor
     move_cursor_to bounds.height + 12
 
-    image open("http://res.cloudinary.com/dq2kcu9ey/image/asset/logo-abe391415fa40d83ff8e3aea6fe6945a.png"), :width => 120, :position => :right
+    image open("http://res.cloudinary.com/dq2kcu9ey/image/asset/logo-abe391415fa40d83ff8e3aea6fe6945a.png"), width: 120, position: :right
 
     move_cursor_to last_measured_y
 
@@ -77,75 +69,61 @@ class InvoicePdf < Prawn::Document
     move_down 65
     last_measured_y = cursor
 
-    text_box " ", :at => [address_x, cursor]
-    move_down lineheight_y
-    text_box @address.full_name_with_salutation, :at => [address_x, cursor]
-    move_down lineheight_y
-    text_box @address.first_line, :at => [address_x, cursor]
-    if @address.second_line.present?
-      move_down lineheight_y
-      text_box @address.second_line, :at => [address_x, cursor]
-    end
-    if @address.third_line.present?
-      move_down lineheight_y
-      text_box @address.third_line, :at => [address_x, cursor]
-    end
-    move_down lineheight_y
-    text_box @address.city_and_postcode, :at => [address_x, cursor]
-    if @address.country.present?
-      move_down lineheight_y
-      text_box @address.convert_country, :at => [address_x, cursor]
-    end
-    if @address.phone_number.present?
-      move_down lineheight_y
-      text_box @address.phone_number, :at => [address_x, cursor]
-    end
+    array = [@address.full_name_with_salutation,
+             @address.first_line,
+             @address.second_line,
+             @address.third_line,
+             @address.city_and_postcode,
+             @address.phone_number]
+
+    many_box(array.compact)
 
     move_cursor_to last_measured_y
+    move_down 20
 
-    table(@invoice_header_data, :position => invoice_header_x, :width => 215) do
-      style(row(0..1).columns(0..1), :padding => [1, 5, 1, 5], :borders => [])
-      # style(row(2), :background_color => 'e9e9e9', :border_color => 'dddddd', :font_style => :bold)
-      style(column(1), :align => :right)
-      # style(row(2).columns(0), :borders => [:top, :left, :bottom])
-      # style(row(2).columns(1), :borders => [:top, :right, :bottom])
+    table(@invoice_header_data, position: invoice_header_x, width: 200) do
+      style(row(0..1).columns(0..1), padding: [1, 5, 1, 5], borders: [])
+      style(column(1), align: :right)
     end
 
     move_down 75
 
-    table(@invoice_services_data, :width => bounds.width) do
-      style(row(1..-1).columns(0..-1), :padding => [4, 5, 4, 5], :borders => [:bottom], :border_color => 'dddddd')
-      style(row(0), :background_color => 'e9e9e9', :border_color => 'dddddd', :font_style => :bold)
-      style(row(0).columns(0..-1), :borders => [:top, :bottom])
-      style(row(0).columns(0), :borders => [:top, :left, :bottom])
-      style(row(0).columns(-1), :borders => [:top, :right, :bottom])
-      style(row(-1), :border_width => 2)
-      style(column(2..-1), :align => :right)
-      style(columns(0), :width => 75)
-      style(columns(1), :width => 275)
+    table(@invoice_services_data, width: 490, position: @address_x) do
+      style(row(1..-1).columns(0..-1), padding: [4, 5, 4, 5], borders: [:bottom], border_color: 'dddddd')
+      style(row(0), background_color: 'e9e9e9', border_color: 'dddddd', font_style: :bold)
+      style(row(0).columns(0..-1), borders: [:top, :bottom])
+      style(row(0).columns(0), borders: [:top, :left, :bottom])
+      style(row(0).columns(-1), borders: [:top, :right, :bottom])
+      style(row(-1), border_width: 2)
+      style(column(2..-1), align: :right)
+      style(columns(0), width: 75)
+      style(columns(1), width: 240)
     end
 
     move_down 1
 
-    table(@invoice_services_totals_data, :position => invoice_header_x, :width => 215) do
-      style(row(0..1).columns(0..1), :padding => [1, 5, 1, 5], :borders => [])
-      style(row(0), :font_style => :bold)
-      style(row(2), :background_color => 'e9e9e9', :border_color => 'dddddd', :font_style => :bold)
-      style(column(1), :align => :right)
-      style(row(2).columns(0), :borders => [:top, :left, :bottom])
-      style(row(2).columns(1), :borders => [:top, :right, :bottom])
+    table(@invoice_services_totals_data, position: invoice_header_x - 15, width: 215) do
+      style(row(0..1).columns(0..1), padding: [1, 5, 1, 5], borders: [])
+      style(row(0), font_style: :bold)
+      style(row(2), background_color: 'e9e9e9', border_color: 'dddddd', font_style: :bold)
+      style(column(1), align: :right)
+      style(row(2).columns(0), borders: [:top, :left, :bottom])
+      style(row(2).columns(1), borders: [:top, :right, :bottom])
     end
 
     move_down 25
 
-    table(@invoice_notes_data, :width => 275) do
-        style(row(0..-1).columns(0..-1), :padding => [10, 0, 10, 0], :borders => [])
-        style(row(0).columns(0), :font_style => :bold)
+    table(@invoice_notes_data, width: 275, position: @address_x) do
+      style(row(0..-1).columns(0..-1), padding: [10, 0, 10, 0], borders: [])
+      style(row(0).columns(0), font_style: :bold)
     end
 
     move_down 10
 
     require 'prawn/icon'
-    icon @icons, inline_format: true
+    # move_right 150
+    bounding_box([40,250], width: 300) do
+      icon @icons, inline_format: true
+    end
   end
 end
